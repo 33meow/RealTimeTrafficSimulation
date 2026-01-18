2c1571a75750f63a6e285c44edbce0077cdb08ab
Bambusx <hess.elias05@gmail.com>
2026-01-18 18:19:50 +0100

    Statistics

--- src/trafficsimulation/GuiController.java
+++ src/trafficsimulation/GuiController.java
@@ -2,6 +2,7 @@ package trafficsimulation;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import javax.swing.SwingUtilities;
 
 public class GuiController {
     
